21a349ce5c4c3a9d51e5c0044b34dc4b5f97c3c8
Nathan Simpson <email.nsimpson@gmail.com>
2022-05-06 11:05:20 +0200

    colours

--- src/plothelp/__init__.py
+++ src/plothelp/__init__.py
@@ -7,8 +7,9 @@ plothelp: Simple auto-grid pyplot util.
 
 from __future__ import annotations
 
+from plothelp.colours import linear_fade, linear_fade_2D
 from plothelp.plot import autogrid
 
 from ._version import version as __version__
 
-__all__ = ("__version__", "autogrid")
+__all__ = ("__version__", "autogrid", "linear_fade", "linear_fade_2D")
