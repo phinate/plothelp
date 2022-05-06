bb06579b2689f30e5cb4ed5c1076ef744dfd3ede
Nathan Simpson <email.nsimpson@gmail.com>
2022-05-06 11:37:41 +0200

    fix tests

--- tests/test_colours.py
+++ tests/test_colours.py
@@ -8,19 +8,19 @@ def test_linear_fade():
     c2 = "blue"
     num_points = 10
     res = linear_fade(c1, c2, num_points)
-    assert res.shape == (3, num_points)
+    assert res.shape == (num_points, 3)
 
     c1 = (1, 0, 0)
     c2 = (0, 0, 1)
     num_points = 10
     res = linear_fade(c1, c2, num_points)
-    assert res.shape == (3, num_points)
+    assert res.shape == (num_points, 3)
 
     c1 = (1, 0, 0, 1)
     c2 = (0, 0, 1, 1)
     num_points = 10
     res = linear_fade(c1, c2, num_points)
-    assert res.shape == (4, num_points)
+    assert res.shape == (num_points, 3)
 
 
 def test_linear_fade_2D():
@@ -30,7 +30,7 @@ def test_linear_fade_2D():
     c4 = "yellow"
     grid_size = (10, 10)
     res = linear_fade_2D(c1, c2, c3, c4, grid_size)
-    assert res.shape == (4, grid_size[0], grid_size[1])
+    assert res.shape == (grid_size[0], grid_size[1], 3)
 
     c1 = (1, 0, 0)
     c2 = (0, 0, 1)
@@ -38,7 +38,7 @@ def test_linear_fade_2D():
     c4 = (1, 1, 0)
     grid_size = (10, 10)
     res = linear_fade_2D(c1, c2, c3, c4, grid_size)
-    assert res.shape == (4, grid_size[0], grid_size[1])
+    assert res.shape == (grid_size[0], grid_size[1], 3)
 
     c1 = (1, 0, 0, 1)
     c2 = (0, 0, 1, 1)
@@ -46,4 +46,4 @@ def test_linear_fade_2D():
     c4 = (1, 1, 0, 1)
     grid_size = (10, 10)
     res = linear_fade_2D(c1, c2, c3, c4, grid_size)
-    assert res.shape == (4, grid_size[0], grid_size[1])
+    assert res.shape == (grid_size[0], grid_size[1], 3)
